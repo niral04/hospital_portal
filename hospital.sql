25a04392b8c40b1b46c1f7899a72e735b928b7bd
Niral Shah <shahniral04@gmail.com>
2025-08-11 22:21:56 +0530

    Add files via upload

---
+++ hospital.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2023 at 07:45 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(11) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `AdminuserName` varchar(20) NOT NULL,
+  `MobileNumber` int(10) NOT NULL,
+  `Email` varchar(120) NOT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `AdminuserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 1234567890, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2023-02-26 18:30:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpatients`
+--
+
+CREATE TABLE `tblpatients` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(12) DEFAULT NULL,
+  `DateOfBirth` date DEFAULT NULL,
+  `GovtIssuedId` varchar(150) DEFAULT NULL,
+  `GovtIssuedIdNo` varchar(11) DEFAULT NULL,
+  `FullAddress` varchar(255) DEFAULT NULL,
+  `State` varchar(200) DEFAULT NULL,
+  `RegistrationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpatients`
+--
+
+INSERT INTO `tblpatients` (`id`, `FullName`, `MobileNumber`, `DateOfBirth`, `GovtIssuedId`, `GovtIssuedIdNo`, `FullAddress`, `State`, `RegistrationDate`) VALUES
+(42, 'Niral Shah', 9322776932, '2004-08-23', 'Aadhar Card', '56784783647', 'Nirant Complex, Kothari Blocks, Bibwewadi, Pune', 'Maharashtra', '2023-05-12 16:36:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblphlebotomist`
+--
+
+CREATE TABLE `tblphlebotomist` (
+  `id` int(11) NOT NULL,
+  `EmpID` varchar(100) DEFAULT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(12) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblphlebotomist`
+--
+
+INSERT INTO `tblphlebotomist` (`id`, `EmpID`, `FullName`, `MobileNumber`, `RegDate`) VALUES
+(1, '12587493', 'Amit Singh', 9876543212, '2023-04-03 04:51:44'),
+(2, '105202365', 'Rahul', 8529631470, '2023-04-03 04:52:06'),
+(3, '10802364', 'Sanjeev Tomar', 1234567890, '2023-04-03 09:34:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblreporttracking`
+--
+
+CREATE TABLE `tblreporttracking` (
+  `id` int(11) NOT NULL,
+  `OrderNumber` bigint(40) DEFAULT NULL,
+  `Remark` varchar(255) DEFAULT NULL,
+  `Status` varchar(120) DEFAULT NULL,
+  `PostingTime` timestamp NULL DEFAULT current_timestamp(),
+  `RemarkBy` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblreporttracking`
+--
+
+INSERT INTO `tblreporttracking` (`id`, `OrderNumber`, `Remark`, `Status`, `PostingTime`, `RemarkBy`) VALUES
+(39, 334658664, 'Will be there at 3 PM', 'On the Way for Collection', '2023-05-12 16:39:15', 1),
+(40, 334658664, 'Sample collected', 'Sample Collected', '2023-05-12 16:39:50', 1),
+(41, 334658664, 'Your sample has been forwarded to lab', 'Sent to Lab', '2023-05-12 16:40:48', 1),
+(42, 334658664, 'Report : Negative ', 'Delivered', '2023-05-12 16:41:45', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbltestrecord`
+--
+
+CREATE TABLE `tbltestrecord` (
+  `id` int(11) NOT NULL,
+  `OrderNumber` bigint(14) DEFAULT NULL,
+  `PatientMobileNumber` bigint(14) DEFAULT NULL,
+  `TestType` varchar(100) DEFAULT NULL,
+  `TestTimeSlot` varchar(120) DEFAULT NULL,
+  `ReportStatus` varchar(100) DEFAULT NULL,
+  `FinalReport` blob DEFAULT NULL,
+  `ReportUploadTime` varchar(200) DEFAULT NULL,
+  `RegistrationDate` timestamp NULL DEFAULT current_timestamp(),
+  `AssignedtoEmpId` varchar(150) DEFAULT NULL,
+  `AssigntoName` varchar(180) DEFAULT NULL,
+  `AssignedTime` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbltestrecord`
+--
+
+INSERT INTO `tbltestrecord` (`id`, `OrderNumber`, `PatientMobileNumber`, `TestType`, `TestTimeSlot`, `ReportStatus`, `FinalReport`, `ReportUploadTime`, `RegistrationDate`, `AssignedtoEmpId`, `AssigntoName`, `AssignedTime`) VALUES
+(43, 334658664, 9322776932, 'Blood', '2023-05-26T16:15', 'Delivered', 0x3836323437636134306632336362393238646661656265363865653862666261313638333930393730352e706466, '12-05-2023 10:11:45 PM', '2023-05-12 16:36:28', '105202365', 'Rahul', '12-05-2023 10:07:32 PM');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpatients`
+--
+ALTER TABLE `tblpatients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblphlebotomist`
+--
+ALTER TABLE `tblphlebotomist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblreporttracking`
+--
+ALTER TABLE `tblreporttracking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbltestrecord`
+--
+ALTER TABLE `tbltestrecord`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblpatients`
+--
+ALTER TABLE `tblpatients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `tblphlebotomist`
+--
+ALTER TABLE `tblphlebotomist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblreporttracking`
+--
+ALTER TABLE `tblreporttracking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `tbltestrecord`
+--
+ALTER TABLE `tbltestrecord`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
